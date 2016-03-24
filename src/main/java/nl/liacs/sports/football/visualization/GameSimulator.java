310fcc1dc5b4d7e847fb6e004a309686cb6a17ff
Roy de Winter <roy-win@hotmail.com>
2016-03-24 11:10:27 +0100

    walls er uit gehaald

--- src/main/java/nl/liacs/sports/football/visualization/GameSimulator.java
+++ src/main/java/nl/liacs/sports/football/visualization/GameSimulator.java
@@ -14,16 +14,10 @@ public class GameSimulator implements Drawable {
     public Vector<Simulatable> simulatables = new Vector<Simulatable>();
     public Field field;
     public Ball ball;
-    // Blocks used to define neutral spots and see if something is hitting it
-    //public Block[] neutralSpots = new Block[4];
-    // This block is not simulated, but exists to define the inner field area
-    //public Block fieldArea;
     public Goal goalLeft, goalRight;
     public PVector fieldCenter;
     float lastT = 0;
-    //	public GoalWall[] goalWalls = new GoalWall[4];
     private Vector<Drawable> drawables = new Vector<Drawable>();
-    //private Block[] walls = new Block[4];
 
     GameSimulator() {
         reset();
@@ -52,10 +46,6 @@ public class GameSimulator implements Drawable {
         field = new Field(FIELD_W, FIELD_H);
         drawables.add(field);
 
-        // Create inner field area
-        //fieldArea = new Block(FIELD_W / 2, FIELD_H / 2, FIELD_W - field.space * 2, FIELD_H - field.space * 2);
-        // drawables.add(fieldArea);
-
         // Save field center
         fieldCenter = new PVector(field.width / 2, field.height / 2);
 
@@ -65,32 +55,12 @@ public class GameSimulator implements Drawable {
         simulatables.add(ball);
         drawables.add(ball);
 
-        //
-        // Make Walls
-        //walls[0] = new Block((FIELD_W / 2), -2f, FIELD_W + 7f, WALL_TICK); // TOP
-        //walls[1] = new Block(FIELD_W / 2, FIELD_H + 2f, FIELD_W + 7f, WALL_TICK); // BOTTOM
-        //walls[2] = new Block(-3f, FIELD_H / 2, WALL_TICK, FIELD_H + WALL_TICK + 4f); // LEFT
-        //walls[3] = new Block(FIELD_W + 3f, FIELD_H / 2, WALL_TICK, FIELD_H + WALL_TICK + 4f); // RIGHT
-
-        for (int i = 0; i < 4; i++) {
-            simulatables.add(walls[i]);
-            drawables.add(walls[i]);
-        }
-
         // Make Neutral Spots
         float xZero = FIELD_W / 2;
         float yZero = FIELD_H / 2;
         float xDelta = (FIELD_W - field.space * 2) / 2;
         float yDelta = field.y_goal / 2;
 
-    /*		neutralSpots[0] = new Block(xZero - xDelta, yZero + yDelta, 0.05f, 0.05f);
-             neutralSpots[1] = new Block(xZero - xDelta, yZero - yDelta, 0.05f, 0.05f);
-     		neutralSpots[2] = new Block(xZero + xDelta, yZero + yDelta, 0.05f, 0.05f);
-     		neutralSpots[3] = new Block(xZero + xDelta, yZero - yDelta, 0.05f, 0.05f);
-     		// for(int i = 0; i < 4; i ++){
-     		// 	drawables.add(neutralSpots[i]);
-     		// }
-     */
         // Make Goals
         xDelta = (FIELD_W - field.space * 2 - field.line_width * 2 + field.x_goal) / 2;
         goalLeft = new Goal(xZero - xDelta, yZero, field.x_goal, field.y_goal);
@@ -100,18 +70,6 @@ public class GameSimulator implements Drawable {
         simulatables.add(goalRight);
         // drawables.add(goalRight);
 
-        // Make Goal Walls
-        //float tickness = field.line_width * 2;
-        //yDelta = (field.y_goal + tickness) / 2;
-        //xDelta = (FIELD_W - field.space * 2 - field.line_width * 2 + field.space) / 2;
-        //goalWalls[0] = new GoalWall(xZero - xDelta, yZero - yDelta, field.space, tickness);
-        //goalWalls[1] = new GoalWall(xZero - xDelta, yZero + yDelta, field.space, tickness);
-        //goalWalls[2] = new GoalWall(xZero + xDelta, yZero - yDelta, field.space, tickness);
-        //goalWalls[3] = new GoalWall(xZero + xDelta, yZero + yDelta, field.space, tickness);
-        //for(int i = 0; i < 4; i ++){
-        // drawables.add(goalWalls[i]);
-        //simulatables.add(goalWalls[i]);
-        //}
     }
 
     public float getTime() {
@@ -139,24 +97,6 @@ public class GameSimulator implements Drawable {
             }
         }
     }
-
-  /*
-		Returns a list of NeutralSpots for the Side given
-   	*/
-  /*
-	public PVector[] getNeutralSpots(TeamSide side){
-   		if(side == TeamSide.LEFT){
-   			return new PVector[]{
-   				neutralSpots[0].getRealPosition(),
-   				neutralSpots[1].getRealPosition()
-   			};
-   		}else{
-   			return new PVector[]{
-   				neutralSpots[2].getRealPosition(),
-   				neutralSpots[3].getRealPosition()
-   			};
-   		}
-   	}
    
    	/*
    		Return neares neutral spot to this position
